4b1e1918f8b171cbe1867eeb5ca75dec514dee4b
Javier Knight <jrcaballerob@gmail.com>
2025-09-23 10:38:12 +0200

    Add: Challenge 4

---
+++ challenges/challenge-4.ts
@@ -0,0 +1,35 @@
+import { getBandValue } from "./challenge-3";
+
+export function getResistorValue(firstColorBand: string, secondColorBand: string, thirdColorBand: string): string {
+    let firstDigit: string|undefined = getBandValue(firstColorBand)?.toString();
+    let secongDigit: string|undefined = getBandValue(secondColorBand)?.toString();
+    let multiplier: number|null = getBandValue(thirdColorBand);
+
+    if (typeof(firstDigit) === 'undefined' || typeof(secongDigit) === 'undefined' || multiplier === null) {
+        return 'NaN';
+    }
+
+    let prefix: string = '';
+    switch(true) {
+        case multiplier < 3:
+            break;
+        case multiplier < 6 || multiplier - 3 === 0:
+            prefix = 'kil';
+            multiplier = multiplier - 3
+            break;
+        case multiplier < 9 || multiplier - 6 === 0:
+            prefix = 'mega';
+            multiplier = multiplier - 6
+            break;
+        case multiplier < 12 || multiplier - 9 === 0:
+            prefix = 'giga';
+            multiplier = multiplier - 9
+            break;
+        default:
+            return 'NaN';
+    }
+
+    let resistanceValue: number = parseInt(firstDigit + secongDigit) * Math.pow(10, multiplier);
+
+    return resistanceValue.toString() + ` ${prefix}ohms`;
+}
---
+++ tests/ch4.test.ts
@@ -0,0 +1,59 @@
+import { getResistorValue } from "../challenges/challenge-4";
+
+const orange: string = 'orange';
+const black: string = 'black';
+const red: string = 'red';
+const yellow: string = 'yellow';
+const blue: string = 'blue';
+const violet: string = 'violet';
+const grey: string = 'grey';
+
+describe ("A challenge about resistor values", () => {
+    test("to look up the numerical value associated with a set of three color bands", () => {
+        let result: string = getResistorValue(orange, orange, black);
+        expect(result).toBe("33 ohms");
+
+        result = getResistorValue(orange, orange, 'brown');
+        expect(result).toBe("330 ohms");
+
+        result = getResistorValue(orange, orange, red);
+        expect(result).toBe("3300 ohms");
+    });
+
+    test("when we get to larger resistors, a metric prefix is used to indicate a larger magnitude of ohms", () => {
+        let result: string | null = getResistorValue(orange, orange, orange);
+        expect(result).toBe("33 kilohms");
+
+        result = getResistorValue(orange, orange, yellow);
+        expect(result).toBe("330 kilohms");
+
+        result = getResistorValue(yellow, red, yellow);
+        expect(result).toBe("420 kilohms");
+
+        result = getResistorValue(orange, orange, yellow);
+        expect(result).toBe("330 kilohms");
+
+        result = getResistorValue(orange, orange, blue);
+        expect(result).toBe("33 megaohms");
+
+        result = getResistorValue(orange, orange, violet);
+        expect(result).toBe("330 megaohms");
+
+        result = getResistorValue(orange, orange, grey);
+        expect(result).toBe("3300 megaohms");
+
+        result = getResistorValue(orange, orange, 'white');
+        expect(result).toBe("33 gigaohms");
+    });
+
+    test("failure cases", () => {
+        let result: string | null = getResistorValue(orange, orange, 'pink');
+        expect(result).toBe("NaN");
+
+        result = getResistorValue('rainbow', orange, 'white');
+        expect(result).toBe("NaN");
+
+        result = getResistorValue(orange, 'typo', 'white');
+        expect(result).toBe("NaN");
+    });
+});
