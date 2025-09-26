bac529452bbb00ba7bd368307fb55a6faf616fd4
Javier Knight <jrcaballerob@gmail.com>
2025-09-26 20:45:56 +0200

    Update: With methods for removing elements from the linked list

--- exercises/medium/excercise-18.ts
+++ exercises/medium/excercise-18.ts
@@ -13,11 +13,13 @@ export class TrainStation {
 
 export class TrainRoute {
     public head: TrainStation | null = null;
+    public tail: TrainStation | null = null;
 
     public addAtTheBeggining(name: string) {
         let newStation = new TrainStation(name);
         if (!this.head) {
             this.head = newStation;
+            this.tail = newStation;
             return;
         }
 
@@ -28,20 +30,15 @@ export class TrainRoute {
 
     public addAtTheEnd(name: string) {
         let newStation = new TrainStation(name);
-        if (!this.head) {
+        if (!this.tail) {
+            this.tail = newStation;
             this.head = newStation;
             return;
         }
 
-        let current: TrainStation | null = this.head;
-
-        //traverseForward
-        while (current.next) {
-            current = current.next;
-        }
-
-        current.next = newStation;
-        newStation.previous = current;
+        this.tail.next = newStation;
+        newStation.previous = this.tail;
+        this.tail = newStation;
     }
 
     public closeStation(name: string) { //remove
@@ -69,35 +66,106 @@ export class TrainRoute {
     }
 
     public travelBarckwards(): string {
-        let stations: string[] = [];
+        let current: TrainStation | null = this.tail;
 
-        let current = this.findLast();
-
-        if (!current) { 
+        if (current === null) {
             return '';
         }
 
+        let stations: string[] = [];
+
         do {
             stations.push(current.name);
-        } while (current = current.previous);
+        } while (current = current.previous)
 
         return stations.join(' -> ');
     }
 
-    public findLast(): TrainStation | null{
+    public removeFirst() {
+        if (!this.head) {
+            return;
+        }
+
+        let current = this.head;
+
+        if (this.head = this.head.next) {
+            this.head.previous = null;
+        }
+
+        if (this.tail === current) { // head and tail were pointing to the same node, clean up tail
+            this.tail = null;
+        }
+    }
+
+    public removeLast() {
+        if (!this.tail) {
+            return;
+        }
+
+        let current = this.tail;
+
+        if (this.tail = this.tail.previous) {
+            this.tail.next = null;
+        }
+
+        if (this.head === current) { // tail and head were pointing to the same node, clean up head
+            this.head = null;
+        }
+    }
+
+    public removeAny(name: string)
+    {
+        let station = this.find(name);
+        
+        if (!station) {
+            return;
+        }
+
+        let previous = station.previous;
+        let next = station.next;
+
+        if (previous === next) { // case with only one element, clean head and tail
+            this.head = this.tail = null;
+            station = previous = next = null;
+            return;
+        }
+
+        if (previous) {
+            previous.next = next;
+        }
+
+        if (next) {
+            next.previous = previous;
+        }
+
+        if (this.head === station) {
+            this.head = next;
+        }
+
+        if (this.tail === station) {
+            this.tail = previous;
+        }
+
+        station = null;
+    }
+
+    public find(name: string): TrainStation | null {
         let current: TrainStation | null = this.head;
 
         if (current === null) {
             return null;
         }
 
-         while (true) {
-            if (current.next === null) {
+        while (true) {
+            if (current.name === name) {
+                return current
+            } else if (current.next === null) {
                 break;
             }
+            
             current = current.next
-         }
+        }
 
-         return current;
+        return null;
     }
 }
--- tests/ex18.test.ts
+++ tests/ex18.test.ts
@@ -1,20 +1,6 @@
-import { TrainRoute } from "../exercises/medium/excercise-18";
+import { TrainRoute, TrainStation } from "../exercises/medium/excercise-18";
 
 describe("double linked lists", () => {
-    test("add at the end and travel in both directions", () => {
-        let route = new TrainRoute();
-
-        route.addAtTheEnd('a1');
-        route.addAtTheEnd('a2');
-        route.addAtTheEnd('a3');
-        route.addAtTheEnd('a4');
-        route.addAtTheEnd('a5');
-        route.addAtTheEnd('a6');
-
-        expect(route.travelForward()).toBe('a1 -> a2 -> a3 -> a4 -> a5 -> a6');
-        expect(route.travelBarckwards()).toBe('a6 -> a5 -> a4 -> a3 -> a2 -> a1');
-    });
-
     test("add at the beginning and travel in both directions", () => {
         let route = new TrainRoute();
 
@@ -27,5 +13,70 @@ describe("double linked lists", () => {
 
         expect(route.travelForward()).toBe('a6 -> a5 -> a4 -> a3 -> a2 -> a1');
         expect(route.travelBarckwards()).toBe('a1 -> a2 -> a3 -> a4 -> a5 -> a6');
+
+        route.removeFirst();
+        expect(route.travelForward()).toBe('a5 -> a4 -> a3 -> a2 -> a1');
+
+        route.removeLast();
+        expect(route.travelForward()).toBe('a5 -> a4 -> a3 -> a2');
+    });
+
+    test("add at the end and travel in both directions", () => {
+        let route = new TrainRoute();
+
+        route.addAtTheEnd('a1');
+        route.addAtTheEnd('a2');
+        route.addAtTheEnd('a3');
+        route.addAtTheEnd('a4');
+        route.addAtTheEnd('a5');
+        route.addAtTheEnd('a6');
+
+        expect(route.travelForward()).toBe('a1 -> a2 -> a3 -> a4 -> a5 -> a6');
+        expect(route.travelBarckwards()).toBe('a6 -> a5 -> a4 -> a3 -> a2 -> a1');
+
+        route.removeAny('a5');
+        expect(route.travelBarckwards()).toBe('a6 -> a4 -> a3 -> a2 -> a1');
+
+        route.removeAny('a3');
+        expect(route.travelBarckwards()).toBe('a6 -> a4 -> a2 -> a1');
+
+        route.removeAny('a6');
+        expect(route.travelBarckwards()).toBe('a4 -> a2 -> a1');
+        expect(route.travelForward()).toBe('a1 -> a2 -> a4');
+
+        route.removeAny('a1');
+        expect(route.travelForward()).toBe('a2 -> a4');
+
+        route.removeAny('a4');
+        expect(route.travelForward()).toBe('a2');
+        expect(route.travelBarckwards()).toBe('a2');
+
+        route.removeAny('a2');
+        expect(route.travelForward()).toBe('');
+        expect(route.travelBarckwards()).toBe('');
+    });
+
+    test('case with only one station, remove from head', () => {
+        let route = new TrainRoute();
+
+        route.addAtTheBeggining('a1');
+        expect(route.travelForward()).toBe('a1');
+        expect(route.travelBarckwards()).toBe('a1');
+
+        route.removeFirst();
+        expect(route.travelForward()).toBe('');
+        expect(route.travelBarckwards()).toBe('');
+    });
+
+    test('case with only one station, remove from tail', () => {
+        let route = new TrainRoute();
+
+        route.addAtTheBeggining('a1');
+        expect(route.travelForward()).toBe('a1');
+        expect(route.travelBarckwards()).toBe('a1');
+
+        route.removeLast();
+        expect(route.travelForward()).toBe('');
+        expect(route.travelBarckwards()).toBe('');
     });
 });
