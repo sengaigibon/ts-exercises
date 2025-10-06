7e18c1b585b23572c912a73203090043c580b44d
Javier Knight <jr.caba@yahoo.com>
2025-10-06 09:44:58 +0200

    Update: Append function adn test

--- exercises/hard/exercise-01.ts
+++ exercises/hard/exercise-01.ts
@@ -1,5 +1,6 @@
 export class List {
     private numbers: number[] = [];
+    private length: number = 0;
 
     public static create(...numbers: number[]) {
         return new List(numbers);
@@ -7,11 +8,24 @@ export class List {
 
     public constructor(numbers: number[]) {
         this.numbers = numbers;
+        this.length = numbers.length;
     }
 
     public forEach(operation: (num: number) => void): void {
         for (let i = 0; i < this.numbers.length; i++) {
             operation(this.numbers[i]);
+        }
+    }
+
+    public getNumbers(): number[] {
+        return this.numbers;
+    }
+
+
+    public append(toAppend: List): void {
+        let nums: number[] = toAppend.getNumbers();
+        for (let i = 0; i < nums.length; i++) {
+            this.numbers[this.length++]= nums[i];
         }
     }
 }
--- tests/hard/ex-01.test.ts
+++ tests/hard/ex-01.test.ts
@@ -4,13 +4,31 @@ describe('Implement basic list operations', () => {
     test('fore each loop', () => {
         const consoleSpy = jest.spyOn(console, 'log').mockImplementation();
 
-        const list = List.create(1, 2, 5)
-        list.forEach((item) => console.log(item))
+        const list = List.create(1, 2, 5);
+        list.forEach((item) => console.log(item));
 
         expect(consoleSpy).toHaveBeenCalledWith(1);
         expect(consoleSpy).toHaveBeenCalledWith(2);
         expect(consoleSpy).toHaveBeenCalledWith(5);
 
         consoleSpy.mockRestore();
-    })
+    });
+
+    test('append: given two lists, add all items in the second list to the end of the first list', () => {
+        const consoleSpy = jest.spyOn(console, 'log').mockImplementation();
+
+        const list1 = List.create(1, 2, 5);
+        const list2 = List.create(4, 3, 0);
+
+        list1.append(list2);
+
+        list1.forEach((item) => console.log(item));
+
+        expect(consoleSpy).toHaveBeenCalledWith(1);
+        expect(consoleSpy).toHaveBeenCalledWith(2);
+        expect(consoleSpy).toHaveBeenCalledWith(5);
+        expect(consoleSpy).toHaveBeenCalledWith(4);
+        expect(consoleSpy).toHaveBeenCalledWith(3);
+        expect(consoleSpy).toHaveBeenCalledWith(0);
+    });
 }) 
