0553c87a5e5f6b958dc2309a3f9d07e733f46658
Javier Knight <jr.caba@yahoo.com>
2025-10-06 16:56:48 +0200

    Add: Other list operations with tests

--- exercises/hard/exercise-01.ts
+++ exercises/hard/exercise-01.ts
@@ -1,12 +1,12 @@
 export class List {
     private numbers: number[] = [];
-    private length: number = 0;
+    public length: number = 0;
 
     public static create(...numbers: number[]) {
         return new List(numbers);
     }
 
-    public constructor(numbers: number[]) {
+    public constructor(numbers: number[] = []) {
         this.numbers = numbers;
         this.length = numbers.length;
     }
@@ -21,11 +21,76 @@ export class List {
         return this.numbers;
     }
 
-
-    public append(toAppend: List): void {
-        let nums: number[] = toAppend.getNumbers();
-        for (let i = 0; i < nums.length; i++) {
-            this.numbers[this.length++]= nums[i];
+    public getItemAt(index: number): number | undefined {
+        if (index < 0 || index >= this.length) {
+            return undefined;
         }
+        return this.numbers[index];
+    }
+
+
+    public append(list: List): void {
+        for (let i = 0; i < list.length; i++) {
+            let item = list.getItemAt(i);
+            this.numbers[this.length++]= item ? item : 0;
+        }
+    }
+
+    public concatenate(...lists: List[]): void {
+        for (let i = 0; i < lists.length; i++) {
+            this.append(lists[i]);
+        }
+    }
+
+    public reverse(): List {
+        let nums: number[] = [];
+        let j: number = 0;
+        for(let i = this.length - 1; i >= 0; i--) {
+            nums[j++] = this.numbers[i];
+        }
+        
+        return new List(nums);
+    }
+
+    public filter(predicate: (num: number) => boolean): List {
+        let nums: number[] = [];
+        let j: number = 0;
+        for (let i = 0; i < this.numbers.length; i++) {
+            if (predicate(this.numbers[i])) {
+                nums[j++] = this.numbers[i];
+            }
+        }
+
+        return new List(nums);
+    }
+
+    public map(func: (num: number) => number): List {
+        let nums: number[] = [];
+        let j: number = 0;
+        for (let i = 0; i < this.numbers.length; i++) {
+            nums[j++] = func(this.numbers[i]);
+        }
+
+        return new List(nums);
+    }
+
+    public foldl(func: (accumulator: number, num: number) => number): number {
+        let acc: number = 0;
+
+        for (let i = 0; i < this.numbers.length; i++) {
+            acc = func(acc, this.numbers[i]);
+        }
+        
+        return acc;
+    }
+
+    public foldr(func: (accumulator: number, num: number) => number): number {
+        let acc: number = 0;
+
+        for(let i = this.length - 1; i >= 0; i--) {
+            acc = func(acc, this.numbers[i]);
+        }
+        
+        return acc;
     }
 }
--- tests/hard/ex-01.test.ts
+++ tests/hard/ex-01.test.ts
@@ -30,5 +30,98 @@ describe('Implement basic list operations', () => {
         expect(consoleSpy).toHaveBeenCalledWith(4);
         expect(consoleSpy).toHaveBeenCalledWith(3);
         expect(consoleSpy).toHaveBeenCalledWith(0);
+
+        consoleSpy.mockRestore();
+    });
+
+    test('concatenate: given a series of lists, combine all items in all lists into one flattened list', () => {
+        const consoleSpy = jest.spyOn(console, 'log').mockImplementation();
+
+        const list1 = List.create(4, 3, 0);
+        const list2 = List.create(1, 2, 5);
+        const list3 = List.create(11, 13, 20, 45);
+        const list4 = List.create(90, 100, 400, 886);
+
+        const flattened = new List();
+        flattened.concatenate(list1, list2, list3, list4);
+
+        flattened.forEach((item) => console.log(item));
+
+        expect(consoleSpy).toHaveBeenCalledWith(4);
+        expect(consoleSpy).toHaveBeenCalledWith(3);
+        expect(consoleSpy).toHaveBeenCalledWith(0);
+        expect(consoleSpy).toHaveBeenCalledWith(1);
+        expect(consoleSpy).toHaveBeenCalledWith(2);
+        expect(consoleSpy).toHaveBeenCalledWith(5);
+        expect(consoleSpy).toHaveBeenCalledWith(11);
+        expect(consoleSpy).toHaveBeenCalledWith(13);
+        expect(consoleSpy).toHaveBeenCalledWith(20);
+        expect(consoleSpy).toHaveBeenCalledWith(45);
+        expect(consoleSpy).toHaveBeenCalledWith(90);
+        expect(consoleSpy).toHaveBeenCalledWith(100);
+        expect(consoleSpy).toHaveBeenCalledWith(400);
+        expect(consoleSpy).toHaveBeenCalledWith(886);
+
+        consoleSpy.mockRestore();
+    });
+
+    test('length: given a list, return the total number of items within it', () => {
+        const consoleSpy = jest.spyOn(console, 'log').mockImplementation();
+
+        const list1 = List.create(4, 3, 0);
+        const list2 = List.create(1, 2, 5);
+        const list3 = List.create(11, 13, 20, 45);
+        const list4 = List.create(90, 100, 400, 886);
+
+        const flattened = new List();
+        flattened.concatenate(list1, list2, list3, list4);
+
+        expect(flattened.length).toBe(14);
+        consoleSpy.mockRestore();
+    });
+
+    test('reverse: given a list, return a list with all the original items, but in reversed order', () => {
+        const list = List.create(90, 100, 400, 886);
+        const reversed = list.reverse();
+        expect(reversed.getNumbers()).toStrictEqual([886, 400, 100, 90]);
+    });
+
+    test('filter: given a predicate and a list, return the list of all items for which predicate(item) is True', () => {
+        const list = List.create(90, 100, 400, 886, 8, 124, 14);
+        let filtered = list.filter(num => num > 100);
+        expect(filtered.getNumbers()).toStrictEqual([400, 886, 124])
+
+        filtered = list.filter(num => num < 100);
+        expect(filtered.getNumbers()).toStrictEqual([90, 8, 14]);
+    });
+
+    test('map: given a function and a list, return the list of the results of applying function(item) on all items', () => {
+        const list = List.create(90, 100, 400, 886);
+        let modified = list.map(num => num / 2);
+        expect(modified.getNumbers()).toStrictEqual([45, 50, 200, 443]);
+
+        modified = list.map(function(num: number) {
+            if (num < 300) {
+                return num * 2;
+            }
+            return num / 2;
+        });
+        expect(modified.getNumbers()).toStrictEqual([180, 200, 200, 443]);
+    });
+
+    test('foldl: given a function, a list, and initial accumulator, fold (reduce) each item into the accumulator from the left', () => {
+        let list = List.create(1, 2, 3, 4);
+        let result = list.foldl((acc, num) => acc + num);
+        expect(result).toBe(10);
+
+        list = List.create(1, 2, 3, -4);
+        result = list.foldl((acc, num) => acc - num);
+        expect(result).toBe(-2);
+    });
+
+    test('foldr: given a function, a list, and an initial accumulator, fold (reduce) each item into the accumulator from the right', () => {
+        let list = List.create(1, 2, 3, 4);
+        let result = list.foldl((acc, num) => acc - num);
+        expect(result).toBe(-10);
     });
 }) 
